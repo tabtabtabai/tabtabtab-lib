46501be374a1b6bb2cff7e42813a46a60f42d7cb
vasin wongrassamee <vasinwr@gmail.com>
2025-04-12 21:58:56 -0700

    make notification obj more standard

--- tabtabtab_lib/extension_interface.py
+++ tabtabtab_lib/extension_interface.py
@@ -1,6 +1,49 @@
 import abc
 from typing import Any, Dict, Optional, Union, List
 from dataclasses import dataclass
+from .sse_interface import SSESenderInterface
+from .llm_interface import LLMProcessorInterface
+import logging
+from enum import Enum
+
+log = logging.getLogger(__name__)
+
+
+class NotificationStatus(Enum):
+    """
+    Enum representing the status of an extension.
+    """
+    PENDING = "pending"           # Extension is running normally
+    READY = "ready"       # Extension is disabled by user/system
+    ERROR = "error"            # Extension encountered an error
+
+
+@dataclass
+class Notification:
+    """
+    Data class representing a push notification.
+    """
+    request_id: str
+    title: str
+    detail: str
+    content: str
+    status: NotificationStatus
+
+    def to_dict(self) -> Dict[str, str]:
+        """
+        Serializes the Notification to a JSON-compatible dictionary.
+
+        Returns:
+            A dictionary containing the notification data.
+        """
+        return {
+            "notification_request_id": self.request_id,
+            "notification_title": self.title,
+            "notification_detail": self.detail, 
+            "notification_content": self.content,
+            "notification_status": self.status.value
+        }
+
 
 
 @dataclass
@@ -9,16 +52,21 @@ class CopyResponse:
     Response object returned by the on_copy method.
 
     Attributes:
-        notification_title: Optional title for a user notification.
-        notification_detail: Optional detailed message for a user notification.
+        notification: Notification object to be sent to the user.
         is_processing_task: Boolean indicating if the extension has started a
                        longer-running background task related to this copy event.
                        Defaults to False.
     """
-
-    notification_title: Optional[str] = None
-    notification_detail: Optional[str] = None
+    notification: Notification
     is_processing_task: bool = False
+
+    def to_dict(self) -> Dict[str, str]:
+        """
+        Serializes the CopyResponse to a JSON-compatible dictionary.
+        """
+        flat_dict = self.notification.to_dict()
+        flat_dict["is_processing_task"] = self.is_processing_task
+        return flat_dict
 
 
 @dataclass
@@ -26,17 +74,22 @@ class PasteResponse:
     """
     Response object returned by the on_paste method.
     """
-
-    notification_title: Optional[str] = None
-    notification_detail: Optional[str] = None
-    paste_content: Optional[str] = None
+    notification: Notification
     is_processing_task: bool = False
 
     def is_accepted(self) -> bool:
         """
         Returns True if the paste request is accepted by the extension.
         """
-        return self.paste_content is not None or self.is_processing_task
+        return self.notification.content is not None or self.is_processing_task
+    
+    def to_dict(self) -> Dict[str, str]:
+        """
+        Serializes the PasteResponse to a JSON-compatible dictionary.
+        """
+        flat_dict = self.notification.to_dict()
+        flat_dict["is_processing_task"] = self.is_processing_task
+        return flat_dict
 
 
 @dataclass
@@ -57,6 +110,7 @@ class OnContextResponse:
     contexts: List[ExtensionContext]
 
 
+
 class ExtensionInterface(abc.ABC):
     """
     Abstract Base Class defining the interface for all TabTabTab extensions.
@@ -64,6 +118,24 @@ class ExtensionInterface(abc.ABC):
     Each extension module must contain a class that inherits from this
     interface and implements all its abstract methods.
     """
+
+    def __init__(
+        self, sse_sender: SSESenderInterface, llm_processor: LLMProcessorInterface, extension_id: str
+    ):
+        """
+        Initializes the SampleExtension.
+
+        Args:
+            sse_sender: An object conforming to the SSESenderInterface
+                        for sending notifications back to the client.
+            llm_processor: An object conforming to the LLMProcessorInterface
+                           for interacting with language models.
+        """
+        self.api_key: Optional[str] = None
+        self.llm_processor = llm_processor  # Store the injected LLM processor
+        self.sse_sender = sse_sender  # Store the injected SSE sender
+        self.extension_id = extension_id
+        log.info(f"[{self.extension_id}] Initializing...")
 
     @abc.abstractmethod
     async def on_context_request(
@@ -130,3 +202,12 @@ class ExtensionInterface(abc.ABC):
             and/or an optional message to notify the user.
         """
         pass
+
+
+    async def send_push_notification(self, device_id: str, notification: Notification) -> None:
+        """
+        Sends a push notification to the user.
+        """
+        notification_dict = notification.to_dict()
+        notification_dict["extension_id"] = self.extension_id
+        await self.sse_sender.send_event(device_id, "extension_notification", notification_dict)
